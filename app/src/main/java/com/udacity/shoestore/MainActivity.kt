10f6d5b8d920f647510498b8dd91afd2822d2e32
DK Tran <d.chan272@gmail.com>
2022-08-13 13:37:26 -0400

    - Enable DataBinding to build.gradle - Add safe-args plugin to build.gradle - Add navigation libraries to build.gradle - Add navigation.xml - Inflate MainActivity.kt with DataBindingUtil - Add ShoeStoreApplication.kt and move Timber init in it

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import timber.log.Timber
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
+
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
