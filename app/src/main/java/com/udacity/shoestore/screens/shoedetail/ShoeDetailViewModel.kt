148b905054da24144aa1d3787c2a19d26fb5292a
DK Tran <d.chan272@gmail.com>
2022-08-17 08:50:50 -0400

    - Add comments to layout xmls - Enable 2-way-binding on fragment_shoedetail.xml

--- app/src/main/java/com/udacity/shoestore/screens/shoedetail/ShoeDetailViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoedetail/ShoeDetailViewModel.kt
@@ -5,11 +5,32 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShoeDetailViewModel : ViewModel() {
+
+    private val _shoeName = MutableLiveData<String>()
+    val shoeName: LiveData<String>
+        get() = _shoeName
+
+    private val _shoeSize = MutableLiveData<Double>()
+    val shoeSize: LiveData<Double>
+        get() = _shoeSize
+
+    private val _company = MutableLiveData<String>()
+    val company: LiveData<String>
+        get() = _company
+
+    private val _description = MutableLiveData<String>()
+    val description: LiveData<String>
+        get() = _shoeName
+
     private val _isFormDirty = MutableLiveData<Boolean>()
     val isFormDirty: LiveData<Boolean>
         get() = _isFormDirty
 
     init {
+        _shoeName.value = null
+        _shoeSize.value = null
+        _company.value = null
+        _description.value = null
         _isFormDirty.value = false
     }
 
